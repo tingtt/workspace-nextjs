77fede0cd53f43949069b3716fb5073807ee1913
tingtt <ting.taku@gmail.com>
2023-02-10 15:13:33 +0900

    feat(hello): update message and style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
     <div className='h-screen flex items-center justify-center'>
-      <div className='text-6xl'>Hello, world!</div>
+      <div className='text-6xl'>
+        Hello,{' '}
+        <span
+          className={`
+            w-[fit-content] text-[4rem] text-black
+            text-opacity-0 bg-[#2B90B6] bg-cover bg-clip-text
+            bg-gradient-to-r from-fuchsia-400 to-fuchsia-600
+          `}
+        >
+          systemd-cd!
+        </span>
+      </div>
     </div>
   )
 }

675df394f00bea9ac3d78a6fa223ee949a4102bc
tingtt <ting.taku@gmail.com>
2023-02-25 11:11:10 +0900

    feat(hello): update message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='h-screen flex items-center justify-center dark:bg-black'>
-      <div className='text-6xl dark:text-white'>Hello, ecc expo!</div>
+      <div className='text-6xl dark:text-white'>Hello, world!</div>
     </div>
   )
 }
